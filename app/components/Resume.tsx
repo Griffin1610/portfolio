14bbaad5462677e0dbd30746352eae58a58f5017
Griffin1610 <PollyGS14@uww.edu>
2025-11-07 15:34:21 -0600

    update resume text content and link

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -1,17 +1,17 @@
-'use client'
+import Link from "next/link";
 import { ResumeIcon } from "./icons";
 
 export default function Resume() {
-
-    const handleClick = (url : string) => {
-        window.open(url, "_blank", "noreferrer");
-    }
-
     return (
         <section className="h-screen pl-[10%] pt-40">
             <h1 className="font-heading text-4xl">resume</h1>
-            <h3 className="font-heading text-xl pt-10 pl-60">Please click below to view my Resume</h3>
-           <ResumeIcon className="w-20 mt-10 cursor-pointer" onClick={() => handleClick("/Polly_Resume.pdf")}/>
+            <h3 className="font-heading text-large pt-2">Please click below to view my Resume</h3>
+            <Link href="/Polly_Resume.pdf" target="_blank" rel="noopener noreferrer">
+                <div className="pl-25 pt-15">
+                    <ResumeIcon className="w-20 cursor-pointer"/>
+                </div>
+            </Link>
+            <h3 className="font-heading text-large pt-15">Thank you for visting my portfolio!</h3>
         </section>
     )
 }
